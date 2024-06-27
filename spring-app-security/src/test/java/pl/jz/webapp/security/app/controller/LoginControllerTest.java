1ad29a5b47097db9c475ba0f3d7cc42c11da7da5
Jacek Zawiślak <zawislak.jacek@gmail.com>
2024-06-27 20:29:01 +0200

    Add test

---
+++ spring-app-security/src/test/java/pl/jz/webapp/security/app/controller/LoginControllerTest.java
@@ -0,0 +1,17 @@
+package pl.jz.webapp.security.app.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void login() {
+    }
+}
---
+++ spring-app-security/src/test/java/pl/jz/webapp/security/app/service/LoginServiceTest.java
@@ -0,0 +1,16 @@
+package pl.jz.webapp.security.app.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LoginServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void login() {
+    }
+}
